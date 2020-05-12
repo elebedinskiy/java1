8f332decffcd1b98faba7dec7ce3dba6edbee478
Evgeniy Lebedinskiy <el@lecorp.ru>
2020-05-12 19:57:39 +0300

    create main class

---
+++ Hello.java
@@ -0,0 +1,14 @@
+package ru.elebedinskiy.java1;
+
+public class Hello
+{
+    public static void main(String[] args)
+    {
+
+        int a = 1;
+        System.out.println("Число a = " + a);
+
+
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-java1
+Java Level 1 <br>
+11-05-2020 <br>
+Evgeniy Lebedinskiy <br>
