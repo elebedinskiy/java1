6dca21844fd7971725d30c5046ca5f7055ff19c1
Evgeniy Lebedinskiy <el@lecorp.ru>
2020-05-15 20:47:16 +0300

    solved task 2 (generate array 0, 3, 6 ...)

--- Hello.java
+++ Hello.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 public class Hello {
 
     public static void main(String[] args) {
+
+        // Задание 1
         int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
         System.out.println("Задание 1:\nИсходный массив: " + Arrays.toString(arr));
         for (int i = 0; i < arr.length; i++){
@@ -15,5 +17,14 @@ public class Hello {
             }
         }
         System.out.println("Изменённый массив: " + Arrays.toString(arr));
+
+        // Задание 2
+        int[] arr2 = new int[8];
+        for (int i = 0, j = 0; i < arr2.length; i++, j = j + 3){
+            arr2[i] = j; // для шага задействована вторая переменная j
+        }
+        System.out.println("\nЗадание 2:\nЗаполненный в цикле массив длинной 8 и шагом 3: " + Arrays.toString(arr2));
+
     }
+
 }
