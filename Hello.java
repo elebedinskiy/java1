610025f297ec7f04afde935056d9e3fef04fde70
Evgeniy Lebedinskiy <el@lecorp.ru>
2020-05-15 20:29:30 +0300

    solved task 1 (convert array)

--- Hello.java
+++ Hello.java
@@ -1,9 +1,19 @@
 package ru.elebedinskiy.java1;
 
+import java.util.Arrays;
+
 public class Hello {
 
     public static void main(String[] args) {
-        // начинаем выполнять задание 2
+        int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        System.out.println("Задание 1:\nИсходный массив: " + Arrays.toString(arr));
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] == 0) {
+                arr[i] = 1;
+            } else if (arr[i] == 1) {
+                arr[i] = 0;
+            }
+        }
+        System.out.println("Изменённый массив: " + Arrays.toString(arr));
     }
-
 }
