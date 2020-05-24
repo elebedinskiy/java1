e5dec45359f6537d39edced2eb7bfc6611a0d5d3
Evgeniy Lebedinskiy <el@lecorp.ru>
2020-05-24 21:06:16 +0300

    add game1 code

--- Hello.java
+++ Hello.java
@@ -8,7 +8,7 @@ public class Hello {
     public static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int userChoice = getNumberFromScanner("Введите: 1 - угадай число, 2 - угадай слово.", 1, 2);
+        int userChoice = getNumbeFromScannerAndValid("Выберите игру: 1 - угадай число, 2 - угадай слово.", 1, 2);
         System.out.println("Вы выбрали = " + userChoice);
         if (userChoice == 1){
             gameOne();
@@ -19,29 +19,59 @@ public class Hello {
     }
 
     public static void gameOne() {
-        // пишем первую игру
-        System.out.println("Первая игра");
+        Random rand = new Random();
+        int secretRundomNumber = rand.nextInt(10); // Задаём случайное число
+        int numberOfAttempts = 3; // Кол-во попыток
 
+        System.out.println("\nЦель игры - угадать случайное число от 0 до 9!\nУ вас " + numberOfAttempts + " попытки");
+
+        for (int i = numberOfAttempts; i >= 0; i--){
+            if (i == 0) {
+                System.out.println("\nПопыток больше нет. Вы проиграли");
+                int userChoice = getNumbeFromScannerAndValid("1 - Играем ещё раз? 0 - нет", 0, 1);
+                if (userChoice == 1){
+                    System.out.println("Отлично");
+                    gameOne();
+                    break;
+                } else {
+                    System.out.println("Пользователь завершил игру.");
+                    break;
+                }
+            }
+            int userEnterNumber = getNumbeFromScannerAndValid("\nВведите число от 0 до 9", 0, 9);
+            System.out.println("Вы ввели = " + userEnterNumber);
+
+            if (userEnterNumber == secretRundomNumber) {
+                System.out.println("Вы угадали! Это победа!");
+                int userChoice = getNumbeFromScannerAndValid("1 - Играем ещё раз? 0 - нет", 0, 1);
+                if (userChoice == 1){
+                    System.out.println("Отлично");
+                    gameOne();
+                    break;
+                } else {
+                    System.out.println("Пользователь завершил игру.");
+                    break;
+                }
+            } else if (userEnterNumber > secretRundomNumber) {
+                System.out.println("Вы ввели слишком большое число.");
+            } else {
+                System.out.println("Вы ввели слишком маленькое число.");
+            }
+        }
     }
 
     public static void gameTwo() {
-        // пишем первую игру
-
-        // Задаём случайное число
-        Random rand = new Random();
-        int secretRundomNumber = rand.nextInt(10);
-
-        System.out.println("Вторая игра");
-
+        System.out.println("Играем в 'Угадай слово'");
+        // Пишем вторую игру
     }
 
-    public static int getNumberFromScanner(String message, int min, int max) {
-        int userNum;
+    public static int getNumbeFromScannerAndValid(String message, int min, int max) {
+        int userEnterNumber; //
         do {
             System.out.println(message);
-            userNum = sc.nextInt();
-        } while (userNum > max || userNum < min);
-        return userNum;
+            userEnterNumber = sc.nextInt();
+        } while (userEnterNumber > max || userEnterNumber < min);
+        return userEnterNumber;
     }
 
 }
