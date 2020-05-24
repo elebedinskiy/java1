f682be7e93129cf4f464a47262236b71e65ad1e9
Evgeniy Lebedinskiy <el@lecorp.ru>
2020-05-24 23:18:26 +0300

    synt

--- Hello.java
+++ Hello.java
@@ -11,178 +11,124 @@ public class Hello {
     static final char DOT_O = 'O';
     static char[][] map;
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         initMap();
         printMap();
 
-        while (true)
-        {
-            humanTurn();
+        while (true) {
+            humanStep();
             printMap();
 
-            if (isWinner(DOT_X))
-            {
+            if (isWinner(DOT_X)) {
                 System.out.println("Победил человек.");
                 break;
             }
-            else if (isMapFull())
-            {
+            else if (isMapFull()) {
                 System.out.println("Ничья.");
                 break;
             }
 
-            aiTurn();
+            pcStep();
             printMap();
 
-            if (isWinner(DOT_O))
-            {
+            if (isWinner(DOT_O)) {
                 System.out.println("Победил компьютер.");
                 break;
             }
-            else if (isMapFull())
-            {
+            else if (isMapFull()) {
                 System.out.println("Ничья.");
                 break;
             }
         }
-
         System.out.println("Игра закончена.");
     }
 
-    static void initMap()
-    {
+    static void initMap() {
         map = new char[SIZE][SIZE];
 
-        for (int i = 0; i < map.length; i++)
-        {
-            for (int j = 0; j < map[i].length; j++)
-            {
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
                 map[i][j] = DOT_EMPTY;
             }
         }
     }
 
-    static void printMap()
-    {
-        for (int i = 0; i <= SIZE; i++)
-        {
-            System.out.print(i + " ");
+    static void printMap() {
+        for (int i = 0; i <= SIZE; i++) {
+            System.out.print(i + "  ");
         }
-
         System.out.println();
 
-        for (int i = 0; i < SIZE; i++)
-        {
-            System.out.print((i + 1) + " ");
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print((i + 1) + "  ");
 
-            for (int j = 0; j < SIZE; j++)
-            {
-                System.out.print(map[i][j] + " ");
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(map[i][j] + "  ");
             }
-
             System.out.println();
         }
-
         System.out.println();
     }
 
-    static void humanTurn()
-    {
-        int x;
-        int y;
+    static void humanStep() {
+        int x, y;
         Scanner scanner = new Scanner(System.in);
 
-        do
-        {
+        do {
             System.out.println("Введите координаты в формате X Y");
-            y = scanner.nextInt() - 1;
             x = scanner.nextInt() - 1;
-        }
-        while (!isCellValid(x, y));
-
+            y = scanner.nextInt() - 1;
+        } while (!isCellValid(x, y));
         map[y][x] = DOT_X;
     }
 
-    static void aiTurn()
-    {
-        int x;
-        int y;
-
+    static void pcStep() {
+        int x, y;
         Random random = new Random();
-
-        do
-        {
+        do {
             x = random.nextInt(SIZE);
             y = random.nextInt(SIZE);
-        }
-        while (!isCellValid(x, y));
+        } while (!isCellValid(x, y));
         System.out.println("Компьютер сходил в точку " + (x + 1) + " " + (y + 1));
         map[y][x] = DOT_O;
     }
 
-    static boolean isCellValid(int x, int y)
-    {
-        if (x < 0 || x >= SIZE || y < 0 || y >= SIZE)
-        {
+    static boolean isCellValid(int x, int y) {
+        if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) {
             return false;
         }
-        if (map[y][x] == DOT_EMPTY)
-        {
+        if (map[y][x] == DOT_EMPTY) {
             return true;
-        }
-        else
-        {
+        } else {
             return false;
         }
     }
 
-    static boolean isWinner(char symbol)
-    {
-        if (map[0][0] == symbol && map[0][1] == symbol && map[0][2] == symbol)
-        {
+    static boolean isWinner(char symbol) {
+        if (map[0][0] == symbol && map[0][1] == symbol && map[0][2] == symbol) {
             return true;
-        }
-        else if (map[1][0] == symbol && map[1][1] == symbol && map[1][2] == symbol)
-        {
+        } else if (map[1][0] == symbol && map[1][1] == symbol && map[1][2] == symbol) {
             return true;
-        }
-        else if (map[2][0] == symbol && map[2][1] == symbol && map[2][2] == symbol)
-        {
+        } else if (map[2][0] == symbol && map[2][1] == symbol && map[2][2] == symbol) {
             return true;
-        }
-        else if (map[0][0] == symbol && map[1][0] == symbol && map[2][0] == symbol)
-        {
+        } else if (map[0][0] == symbol && map[1][0] == symbol && map[2][0] == symbol) {
             return true;
-        }
-        else if (map[0][1] == symbol && map[1][1] == symbol && map[1][2] == symbol)
-        {
+        } else if (map[0][1] == symbol && map[1][1] == symbol && map[1][2] == symbol) {
             return true;
-        }
-        else if (map[0][2] == symbol && map[1][2] == symbol && map[2][2] == symbol)
-        {
+        } else if (map[0][2] == symbol && map[1][2] == symbol && map[2][2] == symbol) {
             return true;
-        }
-        else if (map[0][0] == symbol && map[1][1] == symbol && map[2][2] == symbol)
-        {
+        } else if (map[0][0] == symbol && map[1][1] == symbol && map[2][2] == symbol) {
             return true;
-        }
-        else if (map[0][2] == symbol && map[1][1] == symbol && map[2][1] == symbol)
-        {
+        } else if (map[0][2] == symbol && map[1][1] == symbol && map[2][1] == symbol) {
             return true;
-        }
-        else
-        {
+        } else {
             return false;
         }
     }
 
-    public static boolean isMapFull()
-    {
-        for (int i = 0; i < SIZE; i++)
-        {
-            for (int j = 0; j < SIZE; j++)
-            {
+    public static boolean isMapFull() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
                 if (map[i][j] == DOT_EMPTY) return false;
             }
         }
