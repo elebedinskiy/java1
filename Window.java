3b1a1c8c88e5375a5c4a499ab72fd8bb2fdeef99
Evgeniy Lebedinskiy <el@lecorp.ru>
2020-06-04 22:18:41 +0300

    Создание базового окна

---
+++ Window.java
@@ -0,0 +1,15 @@
+package ru.elebedinskiy.java1;
+
+import javax.swing.*;
+
+public class Window extends JFrame {
+    public Window(){
+        setTitle("Окно Window 1");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(300, 300,640,480);
+        // дописать
+
+        setVisible(true);
+
+    }
+}
