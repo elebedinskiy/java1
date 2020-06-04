4e2cdb98c1cc489c7ad0548a774e8ada83b786f0
Evgeniy Lebedinskiy <el@lecorp.ru>
2020-06-04 23:21:38 +0300

    Повторил, что было сделано на уроке

--- Window.java
+++ Window.java
@@ -1,15 +1,50 @@
 package ru.elebedinskiy.java1;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Window extends JFrame {
     public Window(){
-        setTitle("Окно Window 1");
+        setTitle("Java1: Урок 8. Создание окна");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setBounds(300, 300,640,480);
-        // дописать
+        setBounds(300, 300,400,300);
+
+        // Добавили кнопки и новый слой
+        JButton button1 = new JButton("Что-то делать ...");
+        JButton button2 = new JButton("Выход");
+        JPanel panelButtons = new JPanel();
+
+        // установили для основного слоя компоновщик
+        setLayout(new BorderLayout());
+
+        // Добавили новый слой для расположения кнопок в положение юг
+        add(panelButtons, BorderLayout.SOUTH);
+
+        // установили для нового слоя кнопок другой компоновщик
+        panelButtons.setLayout(new GridLayout());
+
+        // Добавили кнопки на новый слой
+        panelButtons.add(button1);
+        panelButtons.add(button2);
 
         setVisible(true);
 
+        // Обрабочтик события нажатия на кнопку 1
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Что-то делать ...");
+            }
+        });
+
+        // Обрабочтик события нажатия на кнопку 2
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
     }
 }
