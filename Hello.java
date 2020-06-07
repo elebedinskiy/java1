d6b28e7be7bec76ec8c49b4418d435a5a735e7d8
Evgeniy Lebedinskiy <el@lecorp.ru>
2020-06-07 10:02:30 +0300

    fix 10, 11

--- Hello.java
+++ Hello.java
@@ -8,26 +8,23 @@ public class Hello {
         long a4 = 9223372036854775806L; // от -9223372036854775808 до 9223372036854775807, в конце ставим L
         float a5 = 25.804993f; // как int только с плавающей точкой, в конце ставим f
         double a6 = 2; // с плавающей точкой, с двойной точностью
-        char a7 = '*'; // для хранения юникод значений
+        char a7 = '*'; // для хранения юникод значений, размерность 16 бит
         boolean a8 = true; // правда или ложь
-        String a9 = "Строкоеов значение"; // строковая переменная после объявления не может быть изменена
+        String a9 = "Строковое значение"; // строковая переменная после объявления не может быть изменена
 
-        int a, b, c, d;
-        a = 4;
-        b = 6;
-        c = 8;
-        d = -10;
+        float a = 4, b = 6, c = 8, d = -10;
+
         System.out.println("\nЗадание 1: метод возвратит результат вычисления формулы");
-        System.out.println("a * (b + (c / d)) = " + calc(a,b,c,d));
+        System.out.println("a * (b + (c / d)) = " + formulaСalculation(a,b,c,d));
 
         System.out.println("\nЗадание 2: метод возвратит true, если сумма числе неходится между 10 и 20");
-        System.out.println("a = " + a + ", b = " + b + "\nСумма a + b лежит между 10 и 20? -> " + checkSum(a,b));
+        System.out.println("a = " + a + ", b = " + b + "\nСумма a + b лежит между 10 и 20? -> " + isInIntervalFrom10To20(a,b));
 
         System.out.println("\nЗадание 3: метод возвратит true, если число больше или равно 0");
-        System.out.println("Число c = " + c + "\nЧисло положительное? -> " + checkNum(c));
+        System.out.println("Число c = " + c + "\nЧисло положительное? -> " + isAboveZero(c));
 
         System.out.println("\nЗадание 4: метод возвратит true, если число меньше 0");
-        System.out.println("Число d = " + d + "\nЧисло отрицательное? -> " + checkNum2(d));
+        System.out.println("Число d = " + d + "\nЧисло отрицательное? -> " + isLessThanZero(d));
 
         System.out.println("\nЗадание 5: метод возвратит приветствие пользователя по имени");
         String name = "Евгений";
@@ -35,14 +32,14 @@ public class Hello {
 
         System.out.println("\nЗадание 6: метод делает проверку високосный год или нет");
         int year = 2016;
-        System.out.println("Год = " + year + "\nГод високосный? -> " + checkYear(year));
+        System.out.println("Год = " + year + "\nГод високосный? -> " + isLeapYear(year));
 
     }
-    static int calc(int a, int b, int c, int d) {
+    static float formulaСalculation(float a, float b, float c, float d) {
         return a * (b + (c / d));
     }
 
-    static boolean checkSum(int a, int b) {
+    static boolean isInIntervalFrom10To20(float a, float b) {
         boolean i = false;
         if (a + b > 10) {
             if (a + b < 20) {
@@ -52,27 +49,23 @@ public class Hello {
     return i;
     }
 
-    static boolean checkNum(int a) {
-        boolean i = false;
+    static boolean isAboveZero(float a) {
         if (a >= 0) {
-            i = true;
-        }
-        return i;
+            return true;
+        } return false;
     }
 
-    static boolean checkNum2(int a) {
-        boolean i = false;
+    static boolean isLessThanZero(float a) {
         if (a < 0) {
-            i = true;
-        }
-        return i;
+            return true;
+        } return false;
     }
 
     static void helloName(String name){
         System.out.println("Привет, " + name);
     }
 
-    static boolean checkYear(int a) {
+    static boolean isLeapYear(int a) {
         boolean i = false;
         if (a % 100 == 0) {
             i = true;
